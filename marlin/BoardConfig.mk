4c54beb8d7b450e3c81839ff0dc51c765981ad24
Dan Pasanen <dan.pasanen@gmail.com>
2016-12-29 12:07:33 -0600

    marlin: include vendor repo
    * Also add symliks for IMS
    
    Change-Id: I152db0ae552b865accebfaecc26ccfdc7bade270
    
    # Conflicts:
    #	marlin/BoardConfig.mk

--- marlin/BoardConfig.mk
+++ marlin/BoardConfig.mk
@@ -201,3 +201,12 @@ TARGET_COPY_OUT_VENDOR := vendor
 
 #NFC
 NXP_CHIP_TYPE := PN551
+
+#ims
+TARGET_HAS_DEVICE_IMS := true
+
+# GPS
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
+BOARD_VENDOR_QCOM_LOC_PDK_FEATURE_SET := true
+
+-include vendor/google/marlin/BoardConfigVendor.mk
